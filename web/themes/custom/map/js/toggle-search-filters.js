e236d1328d27e36a5d640c0eae24c39d14fe8345
Robert Menetray <robertmenetray@gmail.com>
2020-03-30 20:26:16 +0200

    desactivar la funcion que despliega automaticamente el sidebar de busqueda

--- web/themes/custom/map/js/toggle-search-filters.js
+++ web/themes/custom/map/js/toggle-search-filters.js
@@ -2,24 +2,24 @@
     Drupal.behaviors.toggle_search_filters_sidebar = {
         attach: function (context, settings) {
 
-          $('.view-content.owl-carousel').once('toggle-search-filters-sidebar').each(function(){
-
-            var pathname = window.location.pathname.toString();
-            var referrer = '';
-
-            if(!document.referrer){
-              referrer = pathname;
-            }
-            else{
-              referrer = new URL(document.referrer).pathname.toString();
-            }
-
-            if(referrer !== pathname){
-              //shown on first load
-              $('#sidebar-first').addClass('active');
-            }
-
-          });
+          // $('.view-content.owl-carousel').once('toggle-search-filters-sidebar').each(function(){
+          //
+          //   var pathname = window.location.pathname.toString();
+          //   var referrer = '';
+          //
+          //   if(!document.referrer){
+          //     referrer = pathname;
+          //   }
+          //   else{
+          //     referrer = new URL(document.referrer).pathname.toString();
+          //   }
+          //
+          //   if(referrer !== pathname){
+          //     //shown on first load
+          //     $('#sidebar-first').addClass('active');
+          //   }
+          //
+          // });
 
 
           //show/hidde sidebar
