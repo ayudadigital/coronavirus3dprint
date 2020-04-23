1b7991594a6d2eb1f2728fcb7fcc3986d74837ba
Robert Menetray <robertmenetray@gmail.com>
2020-04-24 00:04:12 +0200

    el sidebar de busqueda siempre se despliega por defecto

--- web/themes/custom/map/js/toggle-search-filters.js
+++ web/themes/custom/map/js/toggle-search-filters.js
@@ -2,24 +2,10 @@
     Drupal.behaviors.toggle_search_filters_sidebar = {
         attach: function (context, settings) {
 
-          // $('.view-content.owl-carousel').once('toggle-search-filters-sidebar').each(function(){
-          //
-          //   var pathname = window.location.pathname.toString();
-          //   var referrer = '';
-          //
-          //   if(!document.referrer){
-          //     referrer = pathname;
-          //   }
-          //   else{
-          //     referrer = new URL(document.referrer).pathname.toString();
-          //   }
-          //
-          //   if(referrer !== pathname){
-          //     //shown on first load
-          //     $('#sidebar-first').addClass('active');
-          //   }
-          //
-          // });
+          $('body').once('toggle-search-filters-sidebar').each(function(){
+            //shown on first load
+            $('#sidebar-first').addClass('active');
+          });
 
 
           //show/hidde sidebar
@@ -27,11 +13,6 @@
             $(this).closest(".sidebar").toggleClass('active');
             return false;
           });
-
-          //hidde sidebar
-          // $('#searchbar-map-wrapper .form-actions button', context).once().bind('click', function() {
-          //     $('#searchbar-map-wrapper', context).removeClass('active');
-          // });
 
         }
     };
